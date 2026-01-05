785f2f7be30011e09508b09cf784d6d0bea2477e
slumbersleep <146052914+slumbersleep@users.noreply.github.com>
2026-01-05 14:51:39 -0800

    Push To Main

---
+++ assets/custom.js
@@ -0,0 +1,80 @@
+jQuery(document).ready(function() {
+    // $('.real-people-slide').slick({
+    //     dots: true,
+    //     infinite: false,
+    //     speed: 300,
+    //     slidesToShow: 5,
+    //     slidesToScroll: 3,
+    //     responsive: [
+    //         {
+    //             breakpoint: 1024,
+    //             settings: {
+    //                 slidesToShow: 3,
+    //                 slidesToScroll: 3,
+    //                 infinite: true,
+    //                 dots: true
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 600,
+    //             settings: {
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 2
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1
+    //             }
+    //         }
+    //     ]
+    // });
+
+    $('.info-slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    dots: false,
+                    arrow: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    dots: false,
+                    arrow: false
+                }
+            }
+        ]
+    });
+
+    setTimeout(() => {
+        $('.header-info-slider').css('visibility', 'visible')
+    }, 1000);
+});
